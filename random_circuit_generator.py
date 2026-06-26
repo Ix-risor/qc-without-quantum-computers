01042c82234bb3e4941f8fe33808fc9eda46b9b9
Edward Rivers <zmac393@live.rhul.ac.uk>
2026-06-26 14:12:28 +0100

    reworked circuit generator so it can provide a set of random circuits as well as the results of those circuits

--- random_circuit_generator.py
+++ random_circuit_generator.py
@@ -1,10 +1,6 @@
 import random
-import matplotlib.pyplot as plt
-import numpy as np
 
 from qiskit.primitives import StatevectorSampler
-from qiskit.visualization import plot_histogram
-from qiskit.circuit.library import CXGate, HGate, SGate, TGate
 from qiskit import QuantumCircuit, generate_preset_pass_manager
 from qiskit.circuit import Gate
 
@@ -17,22 +13,20 @@ def gen_circuit(qubit_count: int, gate_count: int, gate_set: list[Gate]) -> Quan
     qc.measure_all()
     return qc
 
-
-def gen_dataset(circuit_count: int, qubit_count: int, gate_count: int, gate_set: list[Gate]) -> list[dict]:
-    sampler = StatevectorSampler()
-    pm = generate_preset_pass_manager(optimization_level=1)
+def gen_circuit_set(circuit_count: int, qubit_count: int, gate_count: int, gate_set: list[Gate]) -> list[QuantumCircuit]:
     qcs = []
-    output = []
     for _ in range(circuit_count):
-        qc = gen_circuit(qubit_count, gate_count, gate_set)
-        new_qc = pm.run(qc)
-        qcs.append(new_qc)
-    results = sampler.run(qcs).result()
+        qcs.append(gen_circuit(qubit_count, gate_count, gate_set))
+    return qcs
+
+def gen_results(circuit_count: int, qubit_count: int, gate_count: int, gate_set: list[Gate]) -> list[dict]:
+    sampler = StatevectorSampler()
+    pm = generate_preset_pass_manager(optimization_level=2)
+    qcs = gen_circuit_set(circuit_count, qubit_count, gate_count, gate_set)
+    isa_qcs = pm.run(qcs)
+    
+    output = []
+    results = sampler.run(isa_qcs).result()
     for i in range(circuit_count):
         output.append(results[i].data.meas.get_counts())
     return output
-
-data = gen_dataset(10, 5, 12, [HGate(), SGate(), TGate(), CXGate()])
-
-plot_histogram(data)
-plt.show()
